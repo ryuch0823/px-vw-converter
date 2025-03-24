0514488359e6e84811cb2eb4345e0e297a58f940
ryuchi <ryuch0823@gmail.com>
2025-03-24 14:36:59 +0900

    🚀 deploy: update multi-language pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,10 @@ import Footer from "./components/Footer"; // フッターを読み込み
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7371198572486794"
+     crossOrigin="anonymous"></script>
+      </head>
       <body className="min-h-screen flex flex-col">
         <main className="flex-1">{children}</main>
       </body>
