cc23e82a7ed4ded847789a3b5f463e2d7a858eab
ryuchi <ryuch0823@gmail.com>
2025-03-24 21:06:09 +0900

    🚀 deploy: update multi-language pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,24 @@
 "use client";
 
 import "./globals.css";
-import Footer from "./components/Footer"; // フッターを読み込み
+import Script from 'next/script';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+      <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-BG1DKM64JS"
+          strategy="afterInteractive"
+        />
+        <Script id="ga4-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-BG1DKM64JS');
+          `}
+        </Script>
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7371198572486794"
      crossOrigin="anonymous"></script>
       </head>
