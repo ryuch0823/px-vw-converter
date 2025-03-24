9d4a36079a4ac4f5ee0ed967463ea5749a2b6e20
ryuchi <ryuch0823@gmail.com>
2025-03-24 14:31:41 +0900

    🚀 deploy: update multi-language pages

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer className="pt-[3vh] text-xs text-center text-gray-500">
       <Link href="/terms" className="underline mx-2 hover:text-blue-600">Terms</Link>
       <Link href="/privacy" className="underline mx-2 hover:text-blue-600">Privacy Policy</Link>
-      <Link href="/contact" className="underline ml-2 mr-4 hover:text-blue-600">Contact</Link>
+      
       ©2025 <Link href="/" className="underline ml-1 hover:text-blue-600">PX-VW CONVERTER</Link>　
     </footer>
   );
--- app/contact/actions.ts
+++
@@ -1,3 +0,0 @@
-export const sendContactForm = async (formData: any) => {
-    return { success: true }; // ダミー成功レスポンス
-  };
